799a3292c1cd3d56cac486f228181f4845caeb52
Jorge Blanco <jorge.blanco.gil.00@gmail.com>
2022-04-28 21:09:41 +0200

    Added https options

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { existsSync, readFileSync } from 'fs';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const crPath = '/etc/letsencrypt/live/tracknet.one/fullchain.pem';
+  const pkPath = '/etc/letsencrypt/live/tracknet.one/privkey.pem';
+  const options: any = {};
+  if (existsSync(crPath) && existsSync(pkPath)) {
+    // cargamos los archivos sobre las options
+    options.httpsOptions = {
+      cert: readFileSync(crPath),
+      key: readFileSync(pkPath)
+    }
+  }
+  const app = await NestFactory.create(AppModule, options);
+  
   app.setGlobalPrefix('api');
   app.enableCors();
   await app.listen(3000);
