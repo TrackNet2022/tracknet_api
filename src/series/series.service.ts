68dfe4a700dd05f5d0aab67ec80a5af12eb028ed
Jorge Blanco <jorge.blanco.gil.00@gmail.com>
2022-05-13 19:53:01 +0200

    Changed data results

--- src/series/series.service.ts
+++ src/series/series.service.ts
@@ -64,7 +64,7 @@ export class SeriesService {
       const response = this._theMovieDbService
         .getSearchEndpoint()
         .TVShows(args);
-      return response;
+      return (await response).data.results;
     } catch (error) {
       console.log(error);
     }
